b557f60d1eea390a13935977b7f728a2c0bfe83e
zikrie21 <zikri.hadiansyah21@gmail.com>
2022-07-05 23:07:23 +0700

    lab11

---
+++ app/Views/template/footer.php
@@ -0,0 +1,21 @@
+</section>
+<aside id="sidebar">
+    <div class="widget-box">
+        <h3 class="title">Widget Header</h3>
+    <ul>
+        <li><a href="#">Widget Link</a></li>
+        <li><a href="#">Widget Link</a></li>
+    </ul>
+</div>
+<div class="widget-box">
+    <h3 class="title">About Me</h3>
+    <p>Hello Nama Saya ZIKRI HADIANSYAH - 312010069 saya adalah mahasiswa Teknik Informatika di Universitas Pelita Bangsa/p>
+</div>
+</aside>
+</section>
+<footer>
+    <p>&copy; 2021 - Universitas Pelita Bangsa</p>
+</footer>
+</div>
+</body>
+</html>
